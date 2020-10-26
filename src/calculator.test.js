1f8f07bb99c2dec458a71df66e63d01351346e5d
edieatha <edieatha@gmail.com>
2020-10-26 19:15:22 +0300

    Added calculator module tests.

---
+++ src/calculator.test.js
@@ -0,0 +1,17 @@
+const calculator = require('./calculator');
+
+test('add 1 + 2', () => {
+  expect(calculator.add(1, 2)).toBe(3);
+});
+
+test('subtract 1 - 2', () => {
+  expect(calculator.subtract(1, 2)).toBe(-1);
+});
+
+test('divide 4 / 2', () => {
+  expect(calculator.divide(4, 2)).toBe(2);
+});
+
+test('multiply 4 * 2', () => {
+  expect(calculator.multiply(4, 2)).toBe(8);
+});
